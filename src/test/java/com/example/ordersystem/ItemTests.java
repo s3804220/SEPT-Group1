781ce59e95e788329fb09fa83b34c3d85b6c1097
Hanh Doan <s3639869@rmit.edu.vn>
2021-08-05 05:19:47 +0700

    Modify and comment unit test cases for items CRUD

--- src/test/java/com/example/ordersystem/ItemTests.java
+++ src/test/java/com/example/ordersystem/ItemTests.java
@@ -13,6 +13,7 @@ import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+//Reset auto-generated ID back to 1 for accurate testing environment
 @SqlGroup({
         @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, statements="ALTER TABLE items ALTER COLUMN id RESTART WITH 1"),
         @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, statements="ALTER TABLE items ALTER COLUMN id RESTART WITH 1")
@@ -26,6 +27,7 @@ public class ItemTests {
 
     @BeforeEach
     public void init(){
+        //Delete all items in the database before testing
         itemRepository.deleteAll();
         //Add new item before each test case
         Item newItem = new Item("Hot dog","Very hot","dog.jpg",new BigDecimal(3.99));
@@ -39,11 +41,12 @@ public class ItemTests {
 
     @Test
     public void addItemTests(){
+        //Test that item has been added correctly and it exists in the item list
         assertEquals(1,itemService.getAllItems().size());
         assertEquals("Hot dog", itemService.getItem(1L).get().getItemName());
 
-        //Add another item
-        Item newItem2 = new Item("Pie","Yummy pie","food.png|image.jpg",new BigDecimal(7));
+        //Add another item with special characters for a similar test
+        Item newItem2 = new Item("Pie","Yummy pie!!!!&%%%w0w=*","food.png|image.jpg",new BigDecimal(7));
         itemService.saveItem(newItem2);
         assertEquals(2,itemService.getAllItems().size());
         assertEquals(newItem2.getItemDescription(), itemService.getItem(2L).get().getItemDescription());
@@ -52,15 +55,18 @@ public class ItemTests {
     @Test
     public void updateItemTests(){
         Item itemToUpdate = itemService.getItem(1L).get();
+
         //Update the above item's name
         itemToUpdate.setItemName("Super hot dog");
         itemService.saveItem(itemToUpdate);
+        //Test that the item name is correctly updated
         assertEquals("Super hot dog", itemService.getItem(1L).get().getItemName());
     }
 
     @Test
     public void getItemByIdTests(){
         Item itemToGet, itemToGet2, itemToGet3;
+
         //Get an item which is present in the database
         if(itemService.getItem(1L).isPresent()){
             itemToGet = itemService.getItem(1L).get();
@@ -107,6 +113,7 @@ public class ItemTests {
         itemService.saveItem(newItem2);
         Item newItem3 = new Item("Chocolate cake","Sweet and dark","choco.png",new BigDecimal(12));
         itemService.saveItem(newItem3);
+
         //Test that the list reflects the correct size
         assertEquals(3,itemService.getAllItems().size());
         //Delete and test that the change is reflected in the list
