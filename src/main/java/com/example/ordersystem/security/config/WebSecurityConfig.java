9a03c3366126c934070799fe3324ecb6501c8551
s3804220 <s3804220@rmit.edu.vn>
2021-08-06 14:18:53 +0700

    Set default homepage to static index.html

--- src/main/java/com/example/ordersystem/security/config/WebSecurityConfig.java
+++ src/main/java/com/example/ordersystem/security/config/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -26,7 +27,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                    .antMatchers("/registration/**").permitAll()
+                .antMatchers("/").permitAll()
+                .antMatchers("/registration/**").permitAll()
                 .anyRequest()
                 .authenticated().and()
                 .formLogin()
@@ -47,5 +49,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         provider.setPasswordEncoder(bCryptPasswordEncoder);
         provider.setUserDetailsService(accountService);
         return provider;
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/resources/**", "/static/**", "/css/**", "/fonts/**", "/img/**", "/js/**", "/sass/**", "/Source/**");
     }
 }
