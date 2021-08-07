604ec1d69d083ac14f44db0f2ceb477e512faa4f
s3775959 <62049586+s3775959@users.noreply.github.com>
2021-08-07 19:28:30 +0700

    minor fix

--- src/main/java/com/example/ordersystem/config/AppConfig.java
+++ src/main/java/com/example/ordersystem/config/AppConfig.java
@@ -67,9 +67,9 @@ public class AppConfig implements WebMvcConfigurer {
 
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/cosc2101");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("postgres");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/postgres");
+        dataSource.setUsername("pg");
+        dataSource.setPassword("1234");
 //        dataSource.setPassword("super123");
 
 
