73a69d932a12a907bdaf5dc1f550bc4fe76396a6
Hanh Doan <s3639869@rmit.edu.vn>
2021-08-15 23:18:22 +0700

    Add email templates for order confirmed, processed, delivering and delivered status

--- src/main/java/com/example/ordersystem/controller/EmailController.java
+++ src/main/java/com/example/ordersystem/controller/EmailController.java
@@ -3,6 +3,7 @@ package com.example.ordersystem.controller;
 import com.example.ordersystem.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,8 +11,9 @@ public class EmailController {
     @Autowired
     private EmailService emailService;
 
-    @GetMapping(value = "/sendmail")
-    public void sendEmail() {
-        emailService.sendEmail("jefof30612@cytsl.com","created");
+    @GetMapping(value = "/sendmail/{status}")
+    public String sendEmail(@PathVariable String status) {
+        emailService.sendEmail("jefof30612@cytsl.com",status);
+        return "Email sent successfully!";
     }
 }
