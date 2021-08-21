8a2efb975cbcf9e6cf57b73428d42e7f69468d7c
s3804220 <s3804220@rmit.edu.vn>
2021-08-21 20:06:41 +0700

    Add basic VN phone number regex

--- src/main/java/com/example/ordersystem/service/AccountService.java
+++ src/main/java/com/example/ordersystem/service/AccountService.java
@@ -3,6 +3,7 @@ package com.example.ordersystem.service;
 import com.example.ordersystem.exception.account.AccountNotFoundException;
 import com.example.ordersystem.exception.account.EmailAlreadyTakenException;
 import com.example.ordersystem.exception.account.InvalidEmailFormatException;
+import com.example.ordersystem.exception.account.InvalidPhoneFormatException;
 import com.example.ordersystem.model.Account;
 import com.example.ordersystem.model.AccountRole;
 import com.example.ordersystem.repository.AccountRepository;
@@ -28,8 +29,7 @@ public class AccountService implements UserDetailsService {
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     private static final Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
-    private static final String INVALID_EMAIL_ADDRESS = "This email format is invalid";
-
+    private static final Pattern VALID_PHONE_NUMBER_REGEX = Pattern.compile("(0?)(3[2-9]|5[6|8|9]|7[0|6-9]|8[0-6|8|9]|9[0-4|6-9])[0-9]{7}");
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return accountRepository.findByEmail(email)
@@ -42,9 +42,14 @@ public class AccountService implements UserDetailsService {
         if (accountExists){
             throw new EmailAlreadyTakenException(account.getEmail());
         }
-        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(account.getEmail());
-        if (!matcher.find()){
+        Matcher emailMatcher = VALID_EMAIL_ADDRESS_REGEX.matcher(account.getEmail());
+        if (!emailMatcher.find()){
             throw new InvalidEmailFormatException(account.getEmail());
+        }
+
+        Matcher phoneMatcher = VALID_PHONE_NUMBER_REGEX.matcher(account.getPhone());
+        if (!phoneMatcher.find()){
+            throw new InvalidPhoneFormatException(account.getPhone());
         }
         String encodedPassword = bCryptPasswordEncoder.encode(account.getPassword());
         account.setPassword(encodedPassword);
@@ -82,21 +87,17 @@ public class AccountService implements UserDetailsService {
             throw new InvalidEmailFormatException(account.getEmail());
         }
 
-//        String encodedPassword = bCryptPasswordEncoder.encode(account.getPassword());
-//        account.setPassword(encodedPassword);
-//        account.setAccountRole(AccountRole.USER);
-
+        Matcher phoneMatcher = VALID_PHONE_NUMBER_REGEX.matcher(account.getPhone());
+        if (!phoneMatcher.find()){
+            throw new InvalidPhoneFormatException(account.getPhone());
+        }
         accountToUpdate.setFirstName(account.getFirstName());
         accountToUpdate.setLastName(account.getLastName());
         accountToUpdate.setAccAddress(account.getAccAddress());
         accountToUpdate.setPhone(account.getPhone());
         accountToUpdate.setEmail(account.getEmail());
-//        accountToUpdate.setPassword(encodedPassword);
-
 
         accountRepository.save(accountToUpdate);
-//        accountRepository.enableAccount(account.getEmail());
-//        System.out.println(account.getPassword());
         return accountToUpdate;
     }
 
