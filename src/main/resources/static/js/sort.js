cb667967ee251449917f494d7c52e61f5def3eef
kimjunyun7 <kimjunyun7@gmail.com>
2021-08-24 15:52:46 +0700

    Update sort.js

--- src/main/resources/static/js/sort.js
+++ src/main/resources/static/js/sort.js
@@ -1,6 +1,11 @@
 $(function() {
+    let sortField = document.getElementById('sortField');
+
     if (localStorage.getItem('selectedOption')) {
-        $("#sortField option").eq(localStorage.getItem('selectedOption')).prop('selected', true);
+
+        sortField.value = $("#sortField option").eq(localStorage.getItem('selectedOption')).val();
+        $("#sortField").niceSelect('update');
+
     }
     $('#sortField').on('change', function() {
         localStorage.setItem('selectedOption', $('option:selected', this).index());
