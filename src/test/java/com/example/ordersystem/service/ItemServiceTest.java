2b6dc47d08df3fa24d2a7879ea35716be0bd6db2
kimjunyun7 <kimjunyun7@gmail.com>
2021-09-05 01:16:19 +0700

    Update ItemServiceTest.java

--- src/test/java/com/example/ordersystem/service/ItemServiceTest.java
+++ src/test/java/com/example/ordersystem/service/ItemServiceTest.java
@@ -10,14 +10,16 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 //Reset auto-generated ID back to 1 for accurate testing environment
 @SqlGroup({
-        @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, statements="ALTER TABLE items ALTER COLUMN id RESTART WITH 1"),
-        @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, statements="ALTER TABLE items ALTER COLUMN id RESTART WITH 1")
+        @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, statements = "ALTER TABLE items ALTER COLUMN id RESTART WITH 1"),
+        @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, statements = "ALTER TABLE items ALTER COLUMN id RESTART WITH 1")
 })
 @SpringBootTest
 public class ItemServiceTest {
@@ -27,34 +29,35 @@ public class ItemServiceTest {
     private ItemService itemService;
 
     @BeforeEach
-    public void init(){
+    public void init() {
         //Delete all items in the database before testing
         itemRepository.deleteAll();
         //Add new item before each test case
-        Item newItem = new Item("Hot dog","Very hot","dog.jpg",new BigDecimal("3.99"),"Hotdog",true);
+        Item newItem = new Item("Hot dog", "Very hot", "dog.jpg", new BigDecimal("3.99"), "Hotdog", true);
+
         itemService.saveItem(newItem);
     }
 
     @AfterEach
-    public void clearDatabase(){
+    public void clearDatabase() {
         itemRepository.deleteAll();
     }
 
     @Test
-    public void addItemTests(){
+    public void addItemTests() {
         //Test that item has been added correctly and it exists in the item list
-        assertEquals(1,itemService.getAllItems().size());
+        assertEquals(1, itemService.getAllItems().size());
         assertEquals("Hot dog", itemService.getItem(1L).get().getItemName());
 
         //Add another item with special characters for a similar test
-        Item newItem2 = new Item("Pie","Yummy pie!!!!&%%%w0w=*","food.png|image.jpg",new BigDecimal("7"),"Pie",true);
+        Item newItem2 = new Item("Pie", "Yummy pie!!!!&%%%w0w=*", "food.png|image.jpg", new BigDecimal("7"), "Pie", true);
         itemService.saveItem(newItem2);
-        assertEquals(2,itemService.getAllItems().size());
+        assertEquals(2, itemService.getAllItems().size());
         assertEquals(newItem2.getItemDescription(), itemService.getItem(2L).get().getItemDescription());
     }
 
     @Test
-    public void updateItemTests(){
+    public void updateItemTests() {
         Item itemToUpdate = itemService.getItem(1L).get();
 
         //Update the above item's name
@@ -65,44 +68,44 @@ public class ItemServiceTest {
     }
 
     @Test
-    public void getItemByIdTests(){
+    public void getItemByIdTests() {
         Item itemToGet, itemToGet2, itemToGet3;
 
         //Get an item which is present in the database
-        if(itemService.getItem(1L).isPresent()){
+        if (itemService.getItem(1L).isPresent()) {
             itemToGet = itemService.getItem(1L).get();
-        }else {
+        } else {
             itemToGet = null;
         }
         assertNotNull(itemToGet);
         assertEquals(1L, itemToGet.getId().longValue());
 
         //Get an item which is not present in the database
-        if(itemService.getItem(3L).isPresent()){
+        if (itemService.getItem(3L).isPresent()) {
             itemToGet2 = itemService.getItem(3L).get();
-        }else {
+        } else {
             itemToGet2 = null;
         }
         assertNull(itemToGet2);
 
         //Get an item which is not present in the database and have a negative ID
-        if(itemService.getItem(-1L).isPresent()){
+        if (itemService.getItem(-1L).isPresent()) {
             itemToGet3 = itemService.getItem(-1L).get();
-        }else {
+        } else {
             itemToGet3 = null;
         }
         assertNull(itemToGet3);
     }
 
     @Test
-    public void deleteItemTests(){
+    public void deleteItemTests() {
         //Delete item then try to get it by ID
         itemService.deleteItem(1L);
 
         Item itemToGet;
-        if(itemService.getItem(1L).isPresent()){
+        if (itemService.getItem(1L).isPresent()) {
             itemToGet = itemService.getItem(1L).get();
-        }else {
+        } else {
             itemToGet = null;
         }
         assertNull(itemToGet);
@@ -112,67 +115,134 @@ public class ItemServiceTest {
     }
 
     @Test
-    public void getItemsListTests(){
+    public void getItemsListTests() {
         //Add two more items
-        Item newItem2 = new Item("Pie","Yummy pie","food.png|image.jpg",new BigDecimal("7"),"Pie",true);
+        Item newItem2 = new Item("Pie", "Yummy pie", "food.png|image.jpg", new BigDecimal("7"), "Pie", true);
         itemService.saveItem(newItem2);
-        Item newItem3 = new Item("Chocolate cake","Sweet and dark","choco.png",new BigDecimal("12"),"Cake",true);
+        Item newItem3 = new Item("Chocolate cake", "Sweet and dark", "choco.png", new BigDecimal("12"), "Cake", true);
         itemService.saveItem(newItem3);
 
         //Test that the list reflects the correct size
-        assertEquals(3,itemService.getAllItems().size());
+        assertEquals(3, itemService.getAllItems().size());
         //Delete and test that the change is reflected in the list
         itemService.deleteItem(1L);
 
         //Check that the list contains correct information
-        assertEquals(2,itemService.getAllItems().size());
-        assertEquals("Pie",itemService.getAllItems().get(0).getItemName());
-        assertEquals("Yummy pie",itemService.getAllItems().get(0).getItemDescription());
-        assertEquals("food.png|image.jpg",itemService.getAllItems().get(0).getItemImage());
-        assertEquals(new BigDecimal("7.00"),itemService.getAllItems().get(0).getItemPrice());
-        assertEquals("Pie",itemService.getAllItems().get(0).getCategory());
+        assertEquals(2, itemService.getAllItems().size());
+        assertEquals("Pie", itemService.getAllItems().get(0).getItemName());
+        assertEquals("Yummy pie", itemService.getAllItems().get(0).getItemDescription());
+        assertEquals("food.png|image.jpg", itemService.getAllItems().get(0).getItemImage());
+        assertEquals(new BigDecimal("7.00"), itemService.getAllItems().get(0).getItemPrice());
+        assertEquals("Pie", itemService.getAllItems().get(0).getCategory());
         assertTrue(itemService.getAllItems().get(0).isAvailability());
 
         //Test that the list is updated correctly if there is no item in the database
         itemRepository.deleteAll();
-        assertEquals(0,itemService.getAllItems().size());
+        assertEquals(0, itemService.getAllItems().size());
     }
 
-//    @Test
-//    public void findTotalTests() {
-//        Item testItem1 = new Item("testCake1", "Frist test", "product-1.jpg", new BigDecimal("11.00"),"Cake",true);
-//        Item testItem2 = new Item("testCake2","Last test", "product-2.jpg", new BigDecimal("8.00"),"Cake",true);
-//
-//        itemService.saveItem(testItem1);
-//        itemService.saveItem(testItem2);
-//
-//        int amount = 2;
-//        int num = itemService.findTotal();
-//
-//        assertTrue(amount <= itemService.findTotal());
-//        assertEquals(num, itemService.findTotal());
-//    }
 
-//    @Test
-//    public void findListPaging() {
-//        Item testItem1 = new Item("testCake1", "First test", "product-1.jpg", new BigDecimal("11.00"),"Cake",true);
-//        Item testItem2 = new Item("testCake2","Last test", "product-2.jpg", new BigDecimal("8.00"),"Cake",true);
-//
-//        itemService.saveItem(testItem1);
-//        itemService.saveItem(testItem2);
-//
-//        List<Item> itemList = itemService.findListPaging(0, 12);
-//
-//        List<Item> itemList2 = itemService.getAllItems();
-//
-//        int amount = itemList2.size()-1;
-//
-//        assertEquals(itemList2.get(0).getItemName(), itemList.get(0).getItemName());
-//        assertEquals(itemList2.get(0).getItemPrice(), itemList.get(0).getItemPrice());
-//        assertEquals(itemList2.get(0).getItemDescription(), itemList.get(0).getItemDescription());
-//
-//        assertEquals(itemList2.get(amount).getItemName(), itemList.get(amount).getItemName());
-//        assertEquals(itemList2.get(amount).getItemPrice(), itemList.get(amount).getItemPrice());
-//        assertEquals(itemList2.get(amount).getItemDescription(), itemList.get(amount).getItemDescription());
-//    }
+    @Test
+    public void findNumOfSearchedItemsTests() {
+        // Add more items
+        Item newItem2 = new Item("Pie", "Yummy pie", "food.png|image.jpg", new BigDecimal("7"), "Pie", true);
+        Item newItem3 = new Item("Hot chocolate cake", "Sweet and dark", "choco.png", new BigDecimal("12"), "Cake", true);
+        itemService.saveItem(newItem2);
+        itemService.saveItem(newItem3);
+
+        // Conditions
+        String filterField = "All";
+        String searchField = "hot";
+        int numOfItems = itemService.findNumOfSearchedItems(filterField, searchField);
+        List<Item> itemListForTesting = itemService.getAllItems();
+        int numOfItemsInItemList = 0;
+
+
+        for (Item item : itemListForTesting) {
+            if (filterField.equals("All"))
+                if (item.getItemName().toLowerCase().equals(searchField.toLowerCase())) numOfItemsInItemList++;
+             else {
+                if (item.getCategory().equals(filterField))
+                    if (item.getItemName().toLowerCase().equals(searchField.toLowerCase())) numOfItemsInItemList++;
+            }
+        }
+
+        for (Item item : itemListForTesting) {
+            if (item.getItemName().toLowerCase().equals(searchField.toLowerCase())) {
+                if (filterField.equals("All")) {
+                    numOfItemsInItemList++;
+                } else {
+                    if (item.getCategory().equals(filterField)) numOfItemsInItemList++;;
+                }
+            }
+        }
+
+
+        assertEquals(numOfItemsInItemList, numOfItems);
+    }
+
+
+    @Test
+    public void findListPagingTests() {
+        // Add more items
+        Item newItem2 = new Item("Pie", "Yummy pie", "food.png|image.jpg", new BigDecimal("7"), "Pie", true);
+        Item newItem3 = new Item("Hot chocolate cake", "Sweet and dark", "choco.png", new BigDecimal("12"), "Cake", true);
+        itemService.saveItem(newItem2);
+        itemService.saveItem(newItem3);
+
+        // Conditions
+        String filterField = "All";
+        String sortField = "priceHTL";
+        String searchField = "hot";
+        List<Item> itemList = itemService.findListPaging(0, 12, filterField, sortField, searchField);
+        List<Item> fullItemListForTesting = itemService.getAllItems();
+        List<Item> tempItemListForTesting = new ArrayList<>();
+        List<Item> itemListForTesting = new ArrayList<>();
+
+
+
+        for (Item item : fullItemListForTesting) {
+            // Apply search condition
+            if (item.getItemName().toLowerCase().contains(searchField.toLowerCase())) {
+                // Apply filter condition
+                if (filterField.equals("All")) {
+                    tempItemListForTesting.add(item);
+                } else {
+                    if (item.getCategory().equals(filterField)) {
+                        tempItemListForTesting.add(item);
+                    }
+                }
+            }
+        }
+        // Apply sorting condition
+        switch (sortField) {
+            case "id":
+                tempItemListForTesting.sort((o1, o2) -> o1.getId().compareTo(o2.getId()));
+                break;
+
+            case "name":
+                tempItemListForTesting.sort((o1, o2) -> o1.getItemName().compareTo(o2.getItemName()));
+                break;
+
+            case "priceHTL":
+                tempItemListForTesting.sort((o1, o2) -> o2.getItemPrice().compareTo(o1.getItemPrice())); //descending order
+                break;
+
+            case "priceLTH":
+                tempItemListForTesting.sort((o1, o2) -> o1.getItemPrice().compareTo(o2.getItemPrice())); //ascending order
+                break;
+        }
+
+        int loopSize = 0;
+        while (loopSize < tempItemListForTesting.size() && loopSize < 12) {
+            itemListForTesting.add(tempItemListForTesting.get(loopSize));
+            loopSize++;
+        }
+
+
+        assertEquals(itemListForTesting.get(0).getId(), itemList.get(0).getId());
+        assertEquals(itemListForTesting.get(0).getItemName(), itemList.get(0).getItemName());
+        assertEquals(itemListForTesting.size(), itemList.size());
+
+    }
 }
